999bd6a7af6e1c96f287f05c83cc4310d2737a9a
Fortnight <nellis@redhat.com>
2015-08-31 17:58:14 -0400

    Test number 1 does compile, but fails as no rule exists

--- ba-enablement-language/src/main/java/com/redhat/Reason.java
+++ ba-enablement-language/src/main/java/com/redhat/Reason.java
@@ -3,4 +3,12 @@ package com.redhat;
 public class Reason {
 
 	private String reasonMessage; // tell me what happened? was it null?
+
+	public String getReasonMessage() {
+		return reasonMessage;
+	}
+
+	public void setReasonMessage(String reasonMessage) {
+		this.reasonMessage = reasonMessage;
+	}
 }
