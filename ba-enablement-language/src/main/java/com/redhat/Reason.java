7fe1e254b1d557280264ad2fda41c4082ab1407b
Fortnight <nellis@redhat.com>
2015-09-01 13:32:50 -0400

    Added justins changes to the code

--- ba-enablement-language/src/main/java/com/redhat/Reason.java
+++ ba-enablement-language/src/main/java/com/redhat/Reason.java
@@ -11,4 +11,35 @@ public class Reason {
 	public void setReasonMessage(String reasonMessage) {
 		this.reasonMessage = reasonMessage;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((reasonMessage == null) ? 0 : reasonMessage.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Reason other = (Reason) obj;
+		if (reasonMessage == null) {
+			if (other.reasonMessage != null)
+				return false;
+		} else if (!reasonMessage.equals(other.reasonMessage))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Reason [reasonMessage=" + reasonMessage + "]";
+	}
+
 }
